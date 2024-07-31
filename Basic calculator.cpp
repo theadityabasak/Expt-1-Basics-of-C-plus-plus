91c05f1d9da822e506aa5f3b67d881a5bfae9a1c
theadityabasak <orandom46@gmail.com>
2024-08-01 00:54:11 +0530

    Add files via upload
    In this program I created a basic calculator that performs addition, subtraction, multiplication and division

---
+++ Basic calculator.cpp
@@ -0,0 +1,31 @@
+//Aditya Basak
+//PRN:23070123007
+//Basic Calculator Program
+//Experiment- 1
+#include <iostream>
+using namespace std;
+int main(){
+     float division,a,b,sum,difference,product;//intialising variables
+     cout<<"Enter two numbers: ";
+     cin>>a>>b;//take input of two numbers
+     sum = a+b;//calculate sum
+     difference = a-b;//calculate difference
+     product = a*b;//calculate product
+     division = a/b;//calculate division
+     cout<<"sum is: "<<sum<<endl;//display sum
+     cout<<"difference is: "<<difference<<endl;//display difference
+     cout<<"product is: "<<product<<endl;//display product
+     cout<<"division is: "<<division<<endl;//display division
+}
+
+/*
+**OUTPUT**
+Enter two numbers: 2 3
+sum is: 5
+difference is: -1
+product is: 6
+division is: 0.666667 
+*/
+     
+
+   
